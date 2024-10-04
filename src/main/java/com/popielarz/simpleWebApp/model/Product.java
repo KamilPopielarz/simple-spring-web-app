1472f7f7ae0ee217a13497f4679c0f14613e8a0f
Kamil <139891722+KamilPopielarz@users.noreply.github.com>
2024-10-04 14:54:25 +0200

    I finished this episode

--- src/main/java/com/popielarz/simpleWebApp/model/Product.java
+++ src/main/java/com/popielarz/simpleWebApp/model/Product.java
@@ -2,15 +2,19 @@ package com.popielarz.simpleWebApp.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 
 @Data
 @AllArgsConstructor
+@Component
 public class Product {
 
     private int prodId;
     private String prodName;
     private int price;
+
+    public Product(){} //Dlaczego bez tej metody to nie działa? Co ona zmienia? Bez niej wywala błąd, że prodId musi być intem, a przecież jest intem (np 101)??
 
 
 }
--- src/main/java/com/popielarz/simpleWebApp/service/ProductService.java
+++ src/main/java/com/popielarz/simpleWebApp/service/ProductService.java
@@ -1,16 +1,21 @@
 package com.popielarz.simpleWebApp.service;
 
 import com.popielarz.simpleWebApp.model.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(new Product(101, "Iphone 16", 5000), new Product(102, "Iphone 16 plus", 5500));
+    List<Product> products = Arrays.asList(
+            new Product(101, "Iphone 16", 5000),
+            new Product(102, "Iphone 16 plus", 5500),
+            new Product(103, "Iphone 17", 6000));
 
     public List<Product> getProducts() {
-        return null;
+        return products;
     }
 }
