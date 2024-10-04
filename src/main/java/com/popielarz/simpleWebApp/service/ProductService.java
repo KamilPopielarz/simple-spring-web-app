3a289503e0df32f646fb9ef59d558f3152110362
Kamil <139891722+KamilPopielarz@users.noreply.github.com>
2024-10-04 16:06:57 +0200

    Added getProductById method and addProduct method. Now we can search the product by its Id in a link. Also, addProduct gives us an oportunity to  add an item to our products list

--- src/main/java/com/popielarz/simpleWebApp/service/ProductService.java
+++ src/main/java/com/popielarz/simpleWebApp/service/ProductService.java
@@ -10,12 +10,22 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101, "Iphone 16", 5000),
             new Product(102, "Iphone 16 plus", 5500),
-            new Product(103, "Iphone 17", 6000));
+            new Product(103, "Iphone 17", 6000)));
 
     public List<Product> getProducts() {
         return products;
+    }
+
+    public Product getProductsById(int prodId) {
+        return products.stream()
+                .filter(p -> p.getProdId() == prodId)
+                .findFirst().orElse(new Product(100,"No Item",0));
+    }
+
+    public void addProduct(Product prod) {
+        products.add(prod);
     }
 }
