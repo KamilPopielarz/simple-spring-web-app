a8d17970caf6f2254c984318340792e98bd49c52
Kamil <139891722+KamilPopielarz@users.noreply.github.com>
2024-10-04 15:26:45 +0200

    Added NoArgsConstructor in Product class

--- src/main/java/com/popielarz/simpleWebApp/model/Product.java
+++ src/main/java/com/popielarz/simpleWebApp/model/Product.java
@@ -2,19 +2,20 @@ package com.popielarz.simpleWebApp.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 
 @Data
 @AllArgsConstructor
 @Component
+@NoArgsConstructor
 public class Product {
 
     private int prodId;
     private String prodName;
     private int price;
 
-    public Product(){} //Dlaczego bez tej metody to nie działa? Co ona zmienia? Bez niej wywala błąd, że prodId musi być intem, a przecież jest intem (np 101)??
 
 
 }
