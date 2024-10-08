dbb558c98db8793017f3b7b41b71cdf3f3e104dc
Kamil <139891722+KamilPopielarz@users.noreply.github.com>
2024-10-08 22:20:08 +0200

    Added testUpdateProduct in ProductServiceTest class

--- src/test/java/com/popielarz/simpleWebApp/service/ProductServiceTest.java
+++ src/test/java/com/popielarz/simpleWebApp/service/ProductServiceTest.java
@@ -11,35 +11,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProductServiceTest {
 
     @Test
-    public void testGetProducts(){
+    public void testGetProducts() {
         ProductService productService = new ProductService();
         List<Product> products = productService.getProducts();
-        Assertions.assertEquals(3,products.size());
+        Assertions.assertEquals(3, products.size());
     }
 
     @Test
-    public void testGetProductsById(){
+    public void testGetProductsById() {
         ProductService productService = new ProductService();
         Product product103 = productService.getProductsById(103);
-        Assertions.assertEquals(103,product103.getProdId());
-        Assertions.assertEquals("Iphone 17",product103.getProdName());
-        Assertions.assertEquals(6000,product103.getPrice());
+        Assertions.assertEquals(103, product103.getProdId());
+        Assertions.assertEquals("Iphone 17", product103.getProdName());
+        Assertions.assertEquals(6000, product103.getPrice());
     }
 
     @Test
-    public void testGetProductsByIdMissingProduct(){
+    public void testGetProductsByIdMissingProduct() {
         // given
         ProductService productService = new ProductService();
         // when
         Product product103 = productService.getProductsById(-1);
         // then
-        Assertions.assertEquals(100,product103.getProdId());
-        Assertions.assertEquals("No Item",product103.getProdName());
-        Assertions.assertEquals(0,product103.getPrice());
+        Assertions.assertEquals(100, product103.getProdId());
+        Assertions.assertEquals("No Item", product103.getProdName());
+        Assertions.assertEquals(0, product103.getPrice());
     }
 
     @Test
-    public void testAddProduct(){
+    public void testAddProduct() {
         // given
         ProductService productService = new ProductService();
         Product newProduct = new Product(104, "Ip 11 pro max", 4000);
@@ -47,7 +47,7 @@ class ProductServiceTest {
         productService.addProduct(newProduct);
         // then
         List<Product> products = productService.getProducts();
-        Assertions.assertEquals(4,products.size());
+        Assertions.assertEquals(4, products.size());
         Product product104 = productService.getProductsById(104);
         Assertions.assertEquals(104, product104.getProdId());
         Assertions.assertEquals("Ip 11 pro max", product104.getProdName());
@@ -55,19 +55,31 @@ class ProductServiceTest {
     }
 
     @Test
-    public void testDeleteProduct(){
+    public void testDeleteProduct() {
         // given
         ProductService productService = new ProductService();
         // when
         productService.deleteProduct(102);
         // then
         List<Product> products = productService.getProducts();
-        Assertions.assertEquals(2,products.size());
+        Assertions.assertEquals(2, products.size());
         Product product103 = productService.getProductsById(102);
-        Assertions.assertEquals(100,product103.getProdId());
-        Assertions.assertEquals("No Item",product103.getProdName());
-        Assertions.assertEquals(0,product103.getPrice());
+        Assertions.assertEquals(100, product103.getProdId());
+        Assertions.assertEquals("No Item", product103.getProdName());
+        Assertions.assertEquals(0, product103.getPrice());
 
+    }
+
+    @Test
+    public void testUpdateProduct() {
+        // given
+        ProductService productService = new ProductService();
+        Product newProduct = new Product(102, "Ip 11 pro max", 5500);
+        // when
+        productService.updateProduct(newProduct);
+        // then
+        Product product102 = productService.getProductsById(102);
+        Assertions.assertEquals("Ip 11 pro max", product102.getProdName());
     }
 
 
